9ab9a55f49a44d5ea06f6cd3b5fd221ab0a486e8
angel <angelroma92@gmail.com>
2018-04-30 13:03:14 -0600

    Electron Index HTML

---
+++ electronusers/main.js
@@ -0,0 +1,23 @@
+//Aplicacion General
+const app=require('electron').app;
+//Uso de las pantallas del sistema
+const BrowserWindow=require('electron').BrowserWindow;
+//Ruta de carpeta Base
+const path=require('path');
+//URL de las paginas
+const url=require('url');
+//ECMASCRIPT 6-JS
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal=new BrowserWindow({width:620,height:825});
+	PantallaPrincipal.loadURL(url.format({
+		//join: conectar cadenas
+		pathname:path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+	PantallaPrincipal.show();
+}
+
+app.on('ready',muestraPantallaPrincipal);
